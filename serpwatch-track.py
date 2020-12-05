d8cddda26c5812e4695d4ec83c4a05be895ee27e
Tennex <nneal@Tennexs-MBP.home>
2020-12-05 17:32:19 -0500

    add tracking ID lookup

--- serpwatch-track.py
+++ serpwatch-track.py
@@ -5,8 +5,8 @@ import json
 from prettytable import PrettyTable
 
 # call SerpWatch API
-def callSerpAPI(api_key, tracking_id):
-    MANGOOLS_API_URL=f"https://api.mangools.com/v2/serpwatcher/trackings/{tracking_id}"
+def callSerpwatcherTrackingAPI(api_key, tracking_id):
+    API_URL=f"https://api.mangools.com/v2/serpwatcher/trackings/{tracking_id}"
     headers = {
         'accept': 'application/json',
         'x-access-token': api_key
@@ -15,7 +15,7 @@ def callSerpAPI(api_key, tracking_id):
     response = ''
 
     try:
-        response = requests.get(url=MANGOOLS_API_URL, headers=headers, timeout=(5, 5))
+        response = requests.get(url=API_URL, headers=headers, timeout=(5, 5))
     except HTTPError as http_err:
         print("ERROR: There was an HTTP_ERROR on the server side.")
         exit()
@@ -41,9 +41,11 @@ def callSerpAPI(api_key, tracking_id):
     return message
 
 # Parse response from SerpWatch API
-def parseJsonResponse(message):
+def parseTrackingResponse(message):
     data = {}
     data['keywords'] = []
+    data['domain'] = message['tracking']['domain']
+    data['platform'] = "mobile" if message['tracking']['platform_id'] == 2 else "desktop"
     for i in message['keywords']:
         newrow = {}
         newrow['keyword'] = i['kw']
@@ -58,19 +60,86 @@ def parseJsonResponse(message):
     return data
 
 # Output parsed response
-def outputData(data):
-    t = PrettyTable(["keyword", "search_volume", "estimated_visits", "rank_last", "rank_avg", "rank_best"])
+def outputTrackingData(data):
+    print()
+    print(f"Domain: {data['domain']}")
+    print(f"Platform: {data['platform']}")
+    print()
+    #ft = PrettyTable(["domain", "platform"])
+    #ft.add_row([data['domain'], data['platform']])
+    #print(ft)
+
+    t = PrettyTable(["Keyword", "Search Volume", "Estimated Visits", "Rank Last", "Rank Avg.", "Rank Best"])
     for i in data['keywords']:
         t.add_row([i['keyword'],i['search_volume'],i['estimated_visits'],i['rank_last'],i['rank_avg'],i['rank_best']])
 
     print(t)
     return False
 
+def callSerpwatcherLookupAPI(api_key):
+    API_URL=f"https://api.mangools.com/v2/serpwatcher/trackings"
+    headers = {
+        'accept': 'application/json',
+        'x-access-token': api_key
+        }
+
+    response = ''
+
+    try:
+        response = requests.get(url=API_URL, headers=headers, timeout=(5, 5))
+    except HTTPError as http_err:
+        print("ERROR: There was an HTTP_ERROR on the server side.")
+        exit()
+    except SSLError as ssl_err:
+        print("ERROR: There was an SSL_ERROR on the server side.")
+        exit()
+    except ConnectTimeout as ct:
+        print("ERROR: The server was unable to make an HTTP connection.")
+        exit()
+    except ReadTimeout as rt:
+        print("ERROR: The server took too long to respond.")
+        exit()
+    except Exception as err:
+        print("ERROR: An unknown error occured on the server side.")
+        exit()
+    else:
+        if response.status_code == 200:
+            message = response.json()
+        elif response.status_code == 401:
+            print("ERROR: Bad API key")
+            exit()
+
+    return message
+
+def parseLookupResponse(message):
+    data = []
+    for m in message:
+        tmp_data = {}
+        tmp_data['tracking_id'] = m['_id']
+        tmp_data['domain'] = m['domain']
+        tmp_data['platform'] = "mobile" if m['platform_id'] == 2 else "desktop"
+
+        data.append(tmp_data)
+
+    return data
+
+def outputLookupData(data):
+    t = PrettyTable(["Tracking ID", "Domain", "Platform"])
+    for i in data:
+        t.add_row([i['tracking_id'], i['domain'], i['platform']])
+
+    print(t)
+
+    return False
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get keyword data from Mangools SerpWatch API.')
     parser.add_argument('-a', '--api-key', dest='api_key', action='store', required=True, help='This is the API key that Mangools has assigned to you.')
-    parser.add_argument('-t', '--tracking-id', dest='tracking_id', action='store', required=True, help='This is the tracking ID for the list of keywords.')
+    parser.add_argument('-t', '--tracking-id', dest='tracking_id', action='store', default='', help='This is the tracking ID for the list of keywords.')
 
     args = parser.parse_args()
 
-    outputData(parseJsonResponse(callSerpAPI(args.api_key, args.tracking_id)))
+    if args.tracking_id != '':
+        outputTrackingData(parseTrackingResponse(callSerpwatcherTrackingAPI(args.api_key, args.tracking_id)))
+    else:
+        outputLookupData(parseLookupResponse(callSerpwatcherLookupAPI(args.api_key)))
